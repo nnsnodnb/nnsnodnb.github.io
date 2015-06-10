7854b1e209cc3e310d8b8811c7f0b1120be74716
nnsnodnb <oka.03_05@me.com>
2015-06-10 11:33:50 +0900

    Uploaded product2 content

---
+++ three3-ota/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+  <title>Three3-ota</title>
+  <link rel="stylesheet" href="umi/css/bootstrap.min.css">
+  <link rel="stylesheet" href="umi/css/bootstrap-theme.min.css">
+  <script src="umi/js/bootstrap.min.js"></script>
+
+  <style media="screen">
+  html, body {
+    height: 100%;
+  }
+   #footer {
+    height: 60px;
+    background-color: #f5f5f5;
+  }
+  </style>
+</head>
+<body>
+<script type="text/javascript">
+	alert("試験運用中！！");
+</script>
+  <div id="wrap">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">ハロハロみなさん！</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://twitter.com/share" target="new">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>
+      </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+  </div>
+</nav>
+<br><br><br>
+<div class="container">
+  <div class="starter-template">
+    <div align="center">
+      <h1></h1>
+      <img src="tatakatatumuri.jpg" alt="太高祭へようこそ" style="width: 100%;"align="middle">
+    </div>
+  </div>
+</div><!-- /.container -->
+</div>
+
+<div id="footer">
+  <div class="container" align="right">
+    &copy; 2015 <a href="https://twitter.com/nnsnodnb" target="new">nnsnodnb</a>
+  </div>
+
+</div>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
