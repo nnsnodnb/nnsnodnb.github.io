f7e3f8ac4ad2ed98803f69419711d917f5262932
nnsnodnb <nnsnodnb@gmail.com>
2024-02-23 08:39:24 +0900

    Fix routes

--- src/components/CertsCheck.vue
+++ src/components/CertsCheck.vue
@@ -1,12 +1,46 @@
 <template>
+    <div class="container">
+        <a href="https://apps.apple.com/app/certscheck/id6469147491?mt=8" target="_blank" rel="noopener">
+            <div class="card">
+                <img :src="certs_check_appicon" alt="CertsCheck App Icon" class="app_icon">
+            </div>
+        </a>
+    </div>
 </template>
 
 <script>
+import certs_check_appicon from '../assets/img/certs_check_appicon.webp'
+
 export default {
     name: 'CertsCheck',
-    mounted () {
-        window.location = "https://apps.apple.com/app/certscheck/id6469147491?mt=8"
-        window.close()
+    data () {
+        return {
+            certs_check_appicon,
+        }
     },
 }
 </script>
+
+<style scoped>
+.container {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: clip;
+}
+
+.card {
+    box-shadow: 0 4px 15px rgba(0, 0, 0, .2);
+    border-radius: 40px;
+}
+
+.app_icon {
+    width: 120px;
+    height: 120px;
+    margin: 24px;
+    border-radius: 24px;
+}
+
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,43 @@
+<template>
+    <article class="markdown-body">
+        <Header />
+        <ShortCurriculumVitae />
+        <Reward />
+        <Sponcership />
+        <Speaker />
+        <PhotosPosted />
+        <Religion />
+        <UsableTechnology />
+        <DevelopmentRecord />
+        <Contact />
+    </article>
+</template>
+
+<script setup>
+import Header from './Header.vue'
+import ShortCurriculumVitae from './ShortCurriculumVitae.vue'
+import Reward from './Reward.vue'
+import Sponcership from './Sponsorship.vue'
+import Speaker from './Speaker.vue'
+import PhotosPosted from './PhotosPosted.vue'
+import Religion from './Religion.vue'
+import UsableTechnology from './UsableTechnology.vue'
+import DevelopmentRecord from './DevelopmentRecord.vue'
+import Contact from './Contact.vue'
+</script>
+
+<script>
+export default {
+    name: 'Profile',
+}
+</script>
+
+<style scoped>
+.markdown-body {
+    box-sizing: border-box;
+    min-width: 200px;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 45px;
+}
+</style>
