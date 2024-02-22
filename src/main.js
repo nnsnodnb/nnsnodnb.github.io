eb796e68dd227cf158cd65691cd607e73fc0853d
nnsnodnb <nnsnodnb@gmail.com>
2024-02-22 19:58:35 +0900

    Add `/certs-check` route

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import 'github-markdown-css/github-markdown-light.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
