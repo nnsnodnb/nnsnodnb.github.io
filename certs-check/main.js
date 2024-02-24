04f3386304f77c4780f1f9da75b5318598f85223
nnsnodnb <nnsnodnb@gmail.com>
2024-02-24 18:21:56 +0900

    Configure MPA

---
+++ certs-check/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import CertsCheck from '../src/components/CertsCheck.vue'
+
+const app = createApp(CertsCheck)
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                certs_check: resolve(__dirname, 'certs-check//index.html'),
+            }
+        },
+    },
+    appType: 'mpa',
 })
